7768fb8bb1d86fb767d132f8e9a3ef97e8587063
Shirish Shrestha <shrestha_sl@hotmail.com>
2019-12-11 20:00:08 -0600

    initial commit for department api

---
+++ DepartmentEmployees.API/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentEmployees.API.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+
+    }
+}
